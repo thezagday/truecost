6a7803ab929b3923d93578bf31ca52fff6955442
Roman Zagday <roman.zagday@gmail.com>
2020-08-30 00:14:38 +0300

    admiator template base install

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,25 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/dashboard', function () { return view('admin.dashboard'); });
+Route::get('/basic-table', function () { return view('admin.basic-table'); });
+Route::get('/blank', function () { return view('admin.blank'); });
+Route::get('/buttons', function () { return view('admin.buttons'); });
+Route::get('/calendar', function () { return view('admin.calendar'); });
+Route::get('/charts', function () { return view('admin.charts'); });
+Route::get('/chat', function () { return view('admin.chat'); });
+Route::get('/compose', function () { return view('admin.compose'); });
+Route::get('/datatable', function () { return view('admin.datatable'); });
+Route::get('/email', function () { return view('admin.email'); });
+Route::get('/forms', function () { return view('admin.forms'); });
+Route::get('/google-maps', function () { return view('admin.google-maps'); });
+Route::get('/ui', function () { return view('admin.ui'); });
+Route::get('/vector-maps', function () { return view('admin.vector-maps'); });
+
+Route::get('/404', function () { return view('admin.404'); });
+Route::get('/500', function () { return view('admin.500'); });
+
+
 Auth::routes();
 
 Route::group(['middleware' => 'auth'], function() {
